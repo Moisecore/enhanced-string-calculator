9c4b11ca12b5f86f16125455b13127b33436610f
Moisecore <moises.garcia@usach.cl>
2025-10-02 02:13:35 -0300

    added Task 4: Custom Delimiters implemented in F# and Python from Task 3

---
+++ Task 4/Custom-Delimiters.fsx
@@ -0,0 +1,108 @@
+open Microsoft.FSharp.Core.Result
+
+// Type to pass different error messages
+type ErrorMessage =
+    | InputMustNotBeEmpty
+    | InputMustFollowStructure
+    | ElementMustBeInt of int
+    // Error no longer needed | TooManyElements
+    | NoDelimiterFound
+
+// Prompt the user for the input string
+let receiveInput =
+    printfn @"Enter a string containing a delimiter character, and any number of integers separated by the delimiter, 
+    following the format //[delimiter]\n[numbers...]"
+    System.Console.ReadLine()
+
+// Check if input string is empty
+let inputNotEmpty (input:string) =
+    if input = "" then
+        Error InputMustNotBeEmpty
+    else Ok input
+
+// Check if input string follows a valid structure
+let inputStructureValid (input:string) =
+    if input.Contains("//") && input.Contains("\\n") then
+        Ok input
+    else Error InputMustFollowStructure
+
+// Check and validate the input string
+let validateInput input =
+    input
+    |> inputNotEmpty
+    |> bind inputStructureValid
+
+// Get the delimiter from the input string
+let getDelimiter (input:string) = 
+    let partialStrInParts = input.Split("\\n")
+    if partialStrInParts[0].Length < 3 then 
+        ""
+    else 
+        partialStrInParts[0][2..]
+
+// Split the input string by the delimiter
+let splitInput (input:Result<string,ErrorMessage>) =
+    match input with
+        | Ok strInput -> 
+            let delimiter = getDelimiter strInput
+            if delimiter = "" then
+                Error NoDelimiterFound
+            else
+                let numbersPartStr = strInput.Split("\\n")[1]
+                Ok (numbersPartStr.Split(delimiter, System.StringSplitOptions.RemoveEmptyEntries))
+        | Error err -> Error err
+
+// Check if every element splitted from the input string represents an integer
+let validateStrParts (input:array<string>) =
+    let rec checkElement index accum =
+        if index >= input.Length then
+            Ok accum
+        else
+            match System.Int32.TryParse(input[index]) with
+                | true, value -> checkElement (index+1) (value::accum)
+                | false, _ -> Error (ElementMustBeInt (index+1))
+    checkElement 0 []
+
+// Check and validate the values splitted from the input string
+let validateStrArray (input:Result<array<string>,ErrorMessage>) =
+    match input with
+        | Ok strInput -> validateStrParts strInput
+            // Check for up to two numbers no longer needed
+            // if strInput.Length > 2 then
+            //    Error TooManyElements
+            // else validateStrParts strInput
+        | Error err -> Error err
+
+// Sum the numbers parsed from the input string if valid
+let sumNumbers (input:Result<list<int>,ErrorMessage>) : int =
+    match input with
+        | Error err -> 0
+        | Ok numbers -> List.sum numbers
+
+// Returns an int value in accordance with the result of all previous checks and validations
+let returnResult (result:Result<list<int>,ErrorMessage>) : int =
+    match result with
+        | Ok input -> sumNumbers result
+        | Error err ->
+            match err with
+                | InputMustNotBeEmpty -> 0
+                | InputMustFollowStructure -> printfn "Error: the input string does not follow the correct syntax"; 0
+                | ElementMustBeInt index-> printfn "Error: element %i is not an integer" index; 0
+                // No longer an error | TooManyElements -> printfn "ERROR: input must have up to two integers separated by a single comma"; 0
+                | NoDelimiterFound -> printfn "Error: no delimiter found"; 0
+
+// Add function definition
+let add (input:string) : int =
+    input 
+    |> validateInput
+    |> splitInput
+    |> validateStrArray
+    |> returnResult
+
+// General execution of the program
+let inputString = receiveInput
+
+let sum = add inputString
+
+printfn "You entered \"%s\"" inputString
+printfn "The sum is %i" sum
---
+++ Task 4/Custom-Delimiters.py
@@ -0,0 +1,39 @@
+# Add function definition 
+def Add(strIn):
+    # 1st: check if input string is empty
+    if(len(strIn) == 0): return 0
+    # 2nd: check if input string has valid format //[delimiter]\n[numbers...]
+    formatStart = "//"
+    formatMiddle = "\\n"
+    if (formatStart not in strIn) or (formatMiddle not in strIn):
+        print("ERROR: input string does not follow valid format")
+        return 0
+    # 3rd: split input string at the newline (\n) to separate the delimiter and the numbers
+    partialStrInParts = strIn.split("\\n")
+    # 4th: check for delimiter and set it from the input
+    if(len(partialStrInParts[0]) < 3):
+        print("ERROR: input string has no delimiter")
+        return 0
+    delimiter = partialStrInParts[0][2:]
+    numbers = partialStrInParts[1]
+    strInParts = [partial for partial in numbers.split(delimiter)]
+    # 5th: check if the parts of the input string represent integers, and add them if they do
+    index = 0
+    sum = 0
+    for elem in strInParts:
+        try:
+            sum += int(elem)
+            index += 1
+        except:
+            print("ERROR: element " + str(index+1) + " is not an integer")
+            return 0
+    # 6th: return the sum of the integers obtained from the input string
+    return sum
+
+# General execution of the program
+inputString = input("""Enter a string containing a delimiter character, and any number of integers separated by the delimiter,
+following the format //[delimiter]\\n[numbers...]\n""")
+sum = Add(inputString)
+print("You entered \"" + inputString + "\"")
+result = f"{sum}"
+print("The sum is " + result)
